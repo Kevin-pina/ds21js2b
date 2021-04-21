69d73302112462289dce026e9d89f26970cfe978
kevin <kpina@ucol.mx>
2021-04-21 01:10:18 -0500

    first commit

---
+++ 1p/2complexity/2do/complexity-problems.js
@@ -0,0 +1,28 @@
+//
+// create one file by program
+// implement a method, function, non-function solution
+// solve each as requested
+//
+// COMPLEXITY
+
+//1. Least common multiple
+//   lcm(a,b)
+// -> a integer
+// -> b integer
+// <- lcm 
+
+//2. sieve of Eratosthenes
+//   sieve(n)
+// -> n integer
+// <- the n first elements
+
+//3. random number
+//   random(n)
+// -> n integer
+// <- one random number in [1-n]
+
+//4. random name
+//   randomName()
+// -> 
+// <- one random name
+// ie name371
